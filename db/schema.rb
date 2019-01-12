3351c718752578797e8ae0f0b871427d6ee8cab4
javiel <zehua046@gmail.com>
2019-01-12 16:49:02 +0800

    upload resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190103135804) do
+ActiveRecord::Schema.define(version: 20190112081155) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20190103135804) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "job_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
